5bbcbfbc6671e0594551c2dc49145806217eab21
donchodonev <doniodonef@gmail.com>
2021-01-21 13:49:14 +0200

    added 2 final solutions to this part of the course's exercise added exercise's task conditions

--- Conditional-Statements-And-Loops/09. Padawan Equipment/Program.cs
+++ Conditional-Statements-And-Loops/09. Padawan Equipment/Program.cs
@@ -6,7 +6,23 @@ namespace _09._Padawan_Equipment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double moneyAvailable = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double pricePerLightsabre = double.Parse(Console.ReadLine());
+            double pricePerRobe = double.Parse(Console.ReadLine());
+            double pricePerBelt = double.Parse(Console.ReadLine());
+
+            // I see no reason for overbuilding the equation into different variables but it can be refactored for better readability
+            double totalPrice = (pricePerRobe * studentsCount) + (pricePerBelt * (studentsCount - (studentsCount/6))) + (pricePerLightsabre * (Math.Ceiling(studentsCount * 1.10)));
+
+            if (totalPrice <= moneyAvailable)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {totalPrice - moneyAvailable:F2}lv more.");
+            }
         }
     }
 }
