9abc906e177956f2200254bc196318e7af395982
donchodonev <doniodonef@gmail.com>
2021-03-29 09:36:28 +0300

    added all exercise solutions

---
+++ 3.Arrays/03.More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] output = new int[n];
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            for (int i = 0; i < n; i++) //word
+            {
+                string input = Console.ReadLine();
+                int currValue = 0;
+
+                for (int v = 0; v < input.Length; v++) //letter
+                {
+                    bool isVowel = false;
+
+                    for (int c = 0; c < vowels.Length; c++) //check if is vowel 
+                    {
+
+                        if (input[v].ToString().ToLower().Contains(vowels[c]))
+                        {
+                            isVowel = true;
+                        }
+                    }
+                    if (isVowel)
+                    {
+                        currValue += (int)input[v] * input.Length;
+                    }
+                    else
+                    {
+                        currValue += (int)input[v] / input.Length;
+                    }
+                }
+
+                output[i] = currValue;
+            }
+
+            Array.Sort(output);//sort array in ascending order
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 3.Arrays/03.More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int[] currentArray = { 1 };
+            for (int row = 1; row < rows; row++)
+            {
+                int[] nextArray = new int[currentArray.Length + 1];
+                for (int i = 0; i < currentArray.Length; i++)
+                {
+                    nextArray[i] += currentArray[i];
+                    nextArray[i + 1] += currentArray[i];
+                }
+                Console.WriteLine(string.Join(' ', currentArray));
+                currentArray = nextArray;
+            }
+            Console.WriteLine(string.Join(' ', currentArray));
+        }
+    }
+}
