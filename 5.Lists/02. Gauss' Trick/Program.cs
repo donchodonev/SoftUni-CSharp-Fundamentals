48dec1f78be1b37935d2ecdacd5e72a7c55839be
donchodonev <doniodonef@gmail.com>
2021-04-05 17:27:13 +0300

    added Lists folder of exercise solutions added 2 exercises solutions added docx with exercise tasks

---
+++ 5.Lists/02. Gauss' Trick/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int originalCount = input.Count;
+
+            for (int i = 0; i < originalCount / 2; i++)
+            {
+                input[i] += input[input.Count - 1];
+                input.RemoveAt(input.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(' ', input));
+
+        }
+    }
+}
