7b626c431c91bd22be1be15240892d3d4004ca2c
donchodonev <doniodonef@gmail.com>
2021-04-06 12:43:35 +0300

    added 3rd exercise solution

---
+++ 5.Lists/02.Exercise/03. House Party/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = new List<string>(4);
+
+            for (int i = 0; i < n; i++)
+            {
+                bool nameIsOnTheList = false;
+
+                List<string> input = Console.ReadLine().Split().ToList();
+
+                string name = input[0];
+
+                if (names.Contains(name))
+                {
+                    nameIsOnTheList = true;
+                }
+
+                if (input.Count == 3)
+                {
+                    if (nameIsOnTheList)
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        names.Add(name);
+                    }
+                }
+                else
+                {
+                    if (nameIsOnTheList)
+                    {
+                        names.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
