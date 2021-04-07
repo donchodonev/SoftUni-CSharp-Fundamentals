11517b482be69eade5921c3021493da553e15a8c
donchodonev <doniodonef@gmail.com>
2021-04-07 09:59:44 +0300

    added another exercise solution

---
+++ 5.Lists/02.Exercise/07. Append Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> input = Console.ReadLine()
+                .Split('|')
+                .ToList();
+
+            input.Reverse();
+
+            List<string> numbers = new List<string>();
+
+
+            foreach (var item in input)
+            {
+                string[] numbersToAdd = item.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (var num in numbersToAdd)
+                {
+                    numbers.Add(num);
+                }
+            }
+            Console.WriteLine(string.Join(' ',numbers));
+        }
+    }
+}
