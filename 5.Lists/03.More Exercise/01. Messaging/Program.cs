3ffb30afc196948480a8d4ccb6bafb80e844c7b9
donchodonev <doniodonef@gmail.com>
2021-04-09 13:30:14 +0300

    added first exercise solution added exercise task conditions .docx file

---
+++ 5.Lists/03.More Exercise/01. Messaging/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbersAsText = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            string text = Console.ReadLine();
+
+            string finalMessage = string.Empty;
+
+            foreach (var number in numbersAsText)
+            {
+                int numValue = 0;
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    numValue += int.Parse(number[i].ToString());
+                }
+
+                while (numValue > text.Length)
+                {
+                    numValue -= text.Length;
+                }
+
+                finalMessage += text[numValue].ToString();
+                text = text.Remove(numValue, 1);
+            }
+            Console.WriteLine(finalMessage);
+        }
+    }
+}
