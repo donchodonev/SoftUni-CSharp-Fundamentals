2929b30c5ba46ff1f01f04ad316b0ce102bac9b5
donchodonev <doniodonef@gmail.com>
2021-04-11 10:49:33 +0300

    added one more exercise solution

---
+++ 5.Lists/03.More Exercise/04. Mixed up Lists/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Mixed_up_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                             .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToList();
+
+            List<int> secondList = Console.ReadLine()
+                             .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToList();
+
+            //намираме по-малкия лист
+
+            int smallerListCount = Math.Min(firstList.Count, secondList.Count); 
+
+            //декларираме си нов лист, където ще слепим 2та инпут листа
+
+            List<int> combinedList = new List<int>(smallerListCount);
+
+            //обръщаме 2-рия лист, знаем, че ни трябват неговите елементи отзад-напред
+            //по този начин въртим само 1 цикъл
+
+            secondList.Reverse();
+
+            for (int i = 0; i < smallerListCount; i++)
+            {
+                combinedList.Add(firstList[i]);
+                combinedList.Add(secondList[i]);
+            }
+
+            int constraintOne = 0;
+            int constraintTwo = 0;
+
+            if (firstList.Count > secondList.Count)
+            {
+                constraintOne = firstList[firstList.Count - 1];
+                constraintTwo = firstList[firstList.Count - 2];
+            }
+            else
+            {
+                constraintOne = secondList[secondList.Count - 1];
+                constraintTwo = secondList[secondList.Count - 2];
+            }
+
+            //намираме по-голямото/по-малкото на последните 2 елемента,които ще ползваме за констрейнт
+
+            int smallerConstraintNum = Math.Min(constraintTwo, constraintOne);
+            int biggerConstraintNum = Math.Max(constraintTwo, constraintOne);
+
+            List<int> listToPrint = new List<int>(20);
+
+            //пълним си финалния масив само с елементи които влизат между 2-та констрейнта
+
+            listToPrint = combinedList.Where(n => n > smallerConstraintNum && n < biggerConstraintNum).ToList();
+
+            listToPrint.Sort();
+
+            Console.WriteLine(string.Join(" ", listToPrint));
+        }
+    }
+}
