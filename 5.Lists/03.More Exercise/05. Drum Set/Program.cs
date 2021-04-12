bd0828c943aef5181512d3411af27c77521d6d7e
donchodonev <doniodonef@gmail.com>
2021-04-12 10:44:48 +0300

    added final exercise solution

---
+++ 5.Lists/03.More Exercise/05. Drum Set/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _05._Drum_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+
+            List<int> initialSet = Console.ReadLine()
+                .Split(' ',StringSplitOptions.)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> currentSet = new List<int>(6);
+
+            foreach (var item in initialSet)
+            {
+                currentSet.Add(item);
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "Hit it again, Gabsy!")
+            {
+                int power = int.Parse(input);
+
+                List<int> indexOfBrokenDrum = new List<int>(6);
+
+                for (int i = 0; i < currentSet.Count; i++)
+                {
+                    currentSet[i] -= power;
+
+                    if (currentSet[i] <= 0)
+                    {
+                        indexOfBrokenDrum.Add(i);
+                        currentSet.RemoveAt(i);
+                    }
+
+                    foreach (var index in indexOfBrokenDrum)
+                    {
+                        if (initialSet[index] * 3 <= savings)
+                        {
+                            savings -= initialSet[index] * 3;
+                            currentSet.Insert(index, initialSet[index]);
+                            indexOfBrokenDrum.RemoveAt(indexOfBrokenDrum.Count - 1);
+                            break;
+                        }
+                        else
+                        {
+                            initialSet.RemoveAt(i);
+                            indexOfBrokenDrum.RemoveAt(indexOfBrokenDrum.Count - 1);
+
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(' ', currentSet));
+            Console.WriteLine($"Gabsy has {savings:F2}lv.");
+        }
+    }
+}
