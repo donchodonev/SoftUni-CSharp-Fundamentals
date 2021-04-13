f732808f2acc85cb1d5a6b34d38c483b997af9ad
donchodonev <doniodonef@gmail.com>
2021-04-13 12:45:12 +0300

    added Objects and Classes course folder added Lab exercises folder added 2 exercise solutions added Lab exercise .docx tasks file

---
+++ 6.Objects-And-Classes/01.Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Random random = new Random();
+
+            int index = random.Next(0, words.Count);
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string word = words[i];
+                words[i] = words[index];
+                words[index] = word;
+            }
+
+            foreach (var item in words)
+            {
+                Console.WriteLine(item) ;
+            }
+
+        }
+    }
+}
---
+++ 6.Objects-And-Classes/01.Lab/02. Big Factorial/BigIntereger.cs
@@ -0,0 +1,6 @@
+﻿namespace _02._Big_Factorial
+{
+    internal class BigIntereger
+    {
+    }
+}
