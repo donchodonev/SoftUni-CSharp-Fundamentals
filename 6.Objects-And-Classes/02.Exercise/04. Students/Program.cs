c9bfc945031aeb8b1bf23aac1bb960055d6c433a
donchodonev <doniodonef@gmail.com>
2021-04-14 13:27:11 +0300

    added another exercise solution

---
+++ 6.Objects-And-Classes/02.Exercise/04. Students/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade) 
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+
+                Student student =  new Student(input[0], input[1], double.Parse(input[2]));
+
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(x => x.Grade).ToList();
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+}
