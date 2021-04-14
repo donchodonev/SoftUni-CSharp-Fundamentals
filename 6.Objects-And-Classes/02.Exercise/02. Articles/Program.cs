4eacae934a51bc96c2bd8268a4b9019132df85c5
donchodonev <doniodonef@gmail.com>
2021-04-14 09:51:20 +0300

    added another exercise solution

---
+++ 6.Objects-And-Classes/02.Exercise/02. Articles/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Articles
+{
+    class Article
+    {
+        public Article (string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Edit(string content)
+        {
+            Content = content;
+        }
+        public void ChangeAuthor(string author)
+        {
+            Author = author;
+        }
+        public void Rename(string title)
+        {
+            Title = title;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            string title = input[0];
+            string content = input[1];
+            string author = input[2];
+
+            Article article = new Article(title, content, author);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(": ").ToArray();
+
+                if (command [0] == "Edit")
+                {
+                    article.Edit(command[1]);
+                }
+                else if (command[0] == "ChangeAuthor")
+                {
+                    article.ChangeAuthor(command[1]);
+                }
+                else if (command[0] == "Rename")
+                {
+                    article.Rename(command[1]);
+                }
+            }
+            Console.WriteLine(article.ToString());
+        }
+    }
+}
