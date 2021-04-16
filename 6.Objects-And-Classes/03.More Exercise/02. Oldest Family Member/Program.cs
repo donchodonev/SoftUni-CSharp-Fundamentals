554bfc3c4528e19f294909c3633c8c5ec80081d9
donchodonev <doniodonef@gmail.com>
2021-04-16 10:03:33 +0300

    added another exercise solution

---
+++ 6.Objects-And-Classes/03.More Exercise/02. Oldest Family Member/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Oldest_Family_Member
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Family
+    {
+        public Family()
+        {
+        }
+        public List<Person> FamilyMembers { get; set; } = new List<Person>();
+
+        public void AddMember(Person familyMember) 
+        {
+            FamilyMembers.Add(familyMember);
+        }
+        public void GetOldestMember() 
+        {
+            Person oldestPerson = FamilyMembers.OrderByDescending(x => x.Age).First();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split();
+
+                string personName = inputArgs[0];
+                int personAge = int.Parse(inputArgs[1]);
+
+                family.AddMember(new Person(personName, personAge));
+            }
+
+            family.GetOldestMember();
+        }
+    }
+}
