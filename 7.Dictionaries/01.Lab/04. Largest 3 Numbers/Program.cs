49e9a28df14f112ebec6a46c7e544f037574e4a7
donchodonev <doniodonef@gmail.com>
2021-04-20 16:41:57 +0300

    added another exercise solution

---
+++ 7.Dictionaries/01.Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+
+            int[] sorted = input.OrderByDescending(x => x).ToArray();
+
+            if (sorted.Length < 3)
+            {
+                Console.Write(string.Join(' ', sorted));
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(sorted[i] + " ");
+                }
+            }
+        }
+    }
+}
