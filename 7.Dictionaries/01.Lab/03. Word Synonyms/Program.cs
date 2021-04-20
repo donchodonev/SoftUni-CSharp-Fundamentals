1c1ff5c3a5c399878ee0070551db565f690d4ec6
donchodonev <doniodonef@gmail.com>
2021-04-20 16:14:07 +0300

    added another exercise solution

---
+++ 7.Dictionaries/01.Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (words.ContainsKey(word))
+                {
+                    words[word].Add(synonym);
+                }
+                else
+                {
+                    words.Add(word, new List<string>());
+                    words[word].Add(synonym);
+                }
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
