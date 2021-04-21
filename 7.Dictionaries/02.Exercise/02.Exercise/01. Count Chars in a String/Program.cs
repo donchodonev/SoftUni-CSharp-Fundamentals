7b024e08cae89c5f3f28e4079c59d42a4a7373fc
donchodonev <doniodonef@gmail.com>
2021-04-21 11:05:33 +0300

    added first exercise solution added .docx exercise tasks file

---
+++ 7.Dictionaries/02.Exercise/02.Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> chars = new Dictionary<string, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string character = input[i].ToString();
+
+                if (character.Equals(" "))
+                {
+                    continue;
+                }
+
+                if (chars.ContainsKey(character))
+                {
+                    chars[character]++;
+                }
+                else
+                {
+                    chars.Add(character, 1);
+                }
+            }
+
+            foreach (var character in chars)
+            {
+                Console.WriteLine($"{character.Key} -> {character.Value}");
+            }
+        }
+    }
+}
---
+++ 7.Dictionaries/02.Exercise/02.Exercise/02.Exercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
