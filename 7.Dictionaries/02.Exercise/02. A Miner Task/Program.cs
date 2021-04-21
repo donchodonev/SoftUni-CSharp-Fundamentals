07e914360216ce3816491e266eb6207aee3249fc
donchodonev <doniodonef@gmail.com>
2021-04-21 11:25:05 +0300

    added another exercise solution

---
+++ 7.Dictionaries/02.Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string metal = Console.ReadLine();
+
+            Dictionary<string, int> metalQuantity = new Dictionary<string, int>();
+
+            while (metal != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (metalQuantity.ContainsKey(metal))
+                {
+                    metalQuantity[metal] += quantity;
+                }
+                else
+                {
+                    metalQuantity.Add(metal, quantity);
+                }
+
+                metal = Console.ReadLine();
+            }
+
+            foreach (var metalQuantityPair in metalQuantity)
+            {
+                Console.WriteLine($"{metalQuantityPair.Key} -> {metalQuantityPair.Value}");
+            }
+        }
+    }
+}
