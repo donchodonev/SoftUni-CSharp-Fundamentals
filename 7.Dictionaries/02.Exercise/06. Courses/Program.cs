38921924c7e43731b02c4fc85f6930908f55f775
donchodonev <doniodonef@gmail.com>
2021-04-22 11:01:41 +0300

    added another exercise solution

---
+++ 7.Dictionaries/02.Exercise/06. Courses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            string[] input = Console.ReadLine()
+                     .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input.Length > 1)
+            {
+                string courseName = input[0];
+                string studentName = input[1];
+
+                if (courses.ContainsKey(courseName))
+                {
+                    courses[courseName].Add(studentName);
+                }
+                else
+                {
+                    courses.Add(courseName, new List<string> { studentName });
+                }
+
+                input = Console.ReadLine()
+                .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            courses = courses.OrderByDescending(x => x.Value.Count).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count}");
+
+                foreach (var students in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {students}");
+                };
+            }
+        }
+    }
+}
