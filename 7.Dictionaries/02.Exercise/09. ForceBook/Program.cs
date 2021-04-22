71434d55a23120fb59a2a86380c12feb4b3820f6
donchodonev <doniodonef@gmail.com>
2021-04-22 14:38:43 +0300

    updated exercise solution

--- 7.Dictionaries/02.Exercise/09. ForceBook/Program.cs
+++ 7.Dictionaries/02.Exercise/09. ForceBook/Program.cs
@@ -4,91 +4,108 @@ using System.Collections.Generic;
 
 namespace _09._ForceBook
 {
+
     class Program
     {
+        public static void AddUser(Dictionary<string, List<string>> dictionary, string side, string user)
+        {
+            if (dictionary.ContainsKey(side))
+            {
+                if (!dictionary[side].Contains(user))
+                {
+                    dictionary[side].Add(user);
+                }
+            }
+            else
+            {
+                dictionary.Add(side, new List<string> { user });
+            }
+        }
+
         static void Main(string[] args)
         {
-            Dictionary<string, List<string>> forceMemberKVP = new Dictionary<string, List<string>>();
+            Dictionary<string, List<string>> forceSideAndUsers = new Dictionary<string, List<string>>();
 
             string input = Console.ReadLine();
 
-            while (!input.Contains("Lumpawaroo"))
+            while (input != "Lumpawaroo")
             {
-                if (input.Contains("|"))
+
+                if (input.Contains("|")) // means we're adding a user
                 {
-                    string[] inputArgs = input
-                        .Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string[] inputArgs = input.Split(" | ");
 
-
-                    string forceSide = inputArgs[0];
+                    string side = inputArgs[0];
                     string user = inputArgs[1];
-
-                    bool userDoesNotExist = true;
-
-                    foreach (var force in forceMemberKVP)
-                    {
-                        if (!force.Value.Contains(user))
-                        {
-                            userDoesNotExist = true;
-                        }
-                        else
-                        {
-                            userDoesNotExist = false;
-                            break;
-                        }
-                    }
-
-                    if (userDoesNotExist)
-                    {
-                        forceMemberKVP.Add(forceSide, new List<string> { user });
-                    }
-                }
-                else if (input.Contains("->"))
-                {
-                    string[] inputArgs = input
-                         .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
-
-                    string user = inputArgs[0];
-                    string forceSide = inputArgs[1];
 
                     bool userExists = false;
 
-                    foreach (var force in forceMemberKVP)
+                    foreach (var force in forceSideAndUsers)
                     {
                         if (force.Value.Contains(user))
                         {
                             userExists = true;
-                            force.Value.Remove(user);
-                            forceMemberKVP[forceSide].Add(user);
-                            Console.WriteLine($"{user} joins the {forceSide} side!");
                         }
                     }
-                    //check if user does NOT exist
+
                     if (!userExists)
                     {
-                        forceMemberKVP[forceSide].Add(user);
-                        Console.WriteLine($"{user} joins the {forceSide} side!");
+                        AddUser(forceSideAndUsers, side, user);
                     }
                 }
 
+                else if (input.Contains("->")) // means we're switching user sides
+                {
+                    string[] inputArgs = input.Split(" -> ");
+
+                    string user = inputArgs[0];
+                    string side = inputArgs[1];
+
+                    string sideThatHasTheUser = string.Empty;
+
+                    //find which side the user is on
+                    foreach (var kvp in forceSideAndUsers)
+                    {
+                        if (kvp.Value.Contains(user))
+                        {
+                            sideThatHasTheUser = kvp.Key;
+
+                            //remove him from that side
+
+                            forceSideAndUsers[sideThatHasTheUser].Remove(user);
+
+                            //add user according to input
+
+                            AddUser(forceSideAndUsers, side, user);
+
+                            break;
+                        }
+                    }
+
+                    AddUser(forceSideAndUsers, side, user);
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+
                 input = Console.ReadLine();
+                //end of while loop
             }
 
-            Dictionary<string, List<string>> filteredDictionary = forceMemberKVP
-                 .OrderByDescending(x => x.Value.Count)
-                 .ThenBy(x => x.Key)
-                 .ToDictionary(x => x.Key, x => x.Value);
+            //print
 
-            foreach (var side in filteredDictionary)
+            foreach 
+                (var item in forceSideAndUsers
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key)
+                )
             {
-                if (side.Value.Count > 0)
+                if (item.Value.Count > 0)
                 {
-                    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                    Console.WriteLine($"Side: {item.Key}, Members: {item.Value.Count}");
+                }
 
-                    foreach (var member in side.Value.OrderBy(x => x))
-                    {
-                        Console.WriteLine($"! {member}");
-                    }
+                foreach (var member in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"! {member}");
                 }
             }
         }
