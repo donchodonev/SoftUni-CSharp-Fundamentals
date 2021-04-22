120187285b56e5f6540e71ebb8b1d2f84a176238
donchodonev <doniodonef@gmail.com>
2021-04-22 11:01:55 +0300

    added another exercise solution

---
+++ 7.Dictionaries/02.Exercise/07. Student Academy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, double> studentAndGrades = new Dictionary<string, double>();
+
+            List<string> inputAgrs = new List<string>();
+
+            for (int i = 0; i < n*2; i++)
+            {
+                inputAgrs.Add(Console.ReadLine());
+            }
+
+            for (int v = 0; v < inputAgrs.Count; v += 2)
+            {
+                string studentName = inputAgrs[v];
+                double studentGrade = double.Parse(inputAgrs[v + 1]);
+
+                if (studentAndGrades.ContainsKey(studentName))
+                {
+                    studentAndGrades[studentName] = (studentAndGrades[studentName] + studentGrade) / 2;
+                }
+                else
+                {
+                    studentAndGrades.Add(studentName, studentGrade);
+                }
+            }
+
+            Dictionary<string,double> filteredStudents = studentAndGrades
+                .Where(x => x.Value >= 4.50)
+                .OrderByDescending(x => x.Value)
+                .ToDictionary(x => x.Key , x => x.Value);
+
+            foreach (var student in filteredStudents)
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+            }
+        }
+    }
+}
