5a153607fbf470364d21b4f210d5a317b41e1e4c
donchodonev <doniodonef@gmail.com>
2021-04-22 11:02:17 +0300

    added another exercise solution

---
+++ 7.Dictionaries/02.Exercise/08. Company Users/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyAndEmpID = new Dictionary<string, List<string>>();
+
+            string[] inputArgs = Console.ReadLine().Split(" -> ");
+
+            while (inputArgs.Length > 1)
+            {
+                string companyName = inputArgs[0];
+                string employeeID = inputArgs[1];
+
+                if (companyAndEmpID.ContainsKey(companyName))
+                {
+                    if (!companyAndEmpID[companyName].Contains(employeeID))
+                    {
+                        companyAndEmpID[companyName].Add(employeeID);
+                    }
+                }
+                else
+                {
+                    companyAndEmpID.Add(companyName, new List<string> { employeeID });
+                }
+
+                inputArgs = Console.ReadLine().Split(" -> ");
+            }
+
+            companyAndEmpID = companyAndEmpID.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in companyAndEmpID)
+            {
+                Console.WriteLine($"{item.Key}");
+
+                foreach (var empID in item.Value)
+                {
+                    Console.WriteLine($"-- {empID}");
+                }
+            }
+        }
+    }
+}
