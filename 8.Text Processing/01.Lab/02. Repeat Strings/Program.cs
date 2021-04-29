7fa70f3cbc99688e27758ad4812ade4793732e6e
donchodonev <doniodonef@gmail.com>
2021-04-29 10:13:43 +0300

    added another exercise solution

---
+++ 8.Text Processing/01.Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] input = Console.ReadLine().Split();
+
+            foreach (var word in input)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    sb.Append(word);
+                }
+            }
+            Console.WriteLine(sb) ;
+        }
+    }
+}
