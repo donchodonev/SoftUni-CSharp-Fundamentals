29da2cd46bfbb993e12313cbca53a04f056f12da
donchodonev <doniodonef@gmail.com>
2021-04-29 10:25:43 +0300

    added another exercise solution

---
+++ 8.Text Processing/01.Lab/03. Substring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine().ToLowerInvariant();
+            string secondString = Console.ReadLine().ToLowerInvariant();
+
+            int index = secondString.IndexOf(firstString);
+
+            while (index != -1)
+            {
+                secondString = secondString.Remove(index, firstString.Length);
+                index = secondString.IndexOf(firstString);
+            }
+
+            Console.WriteLine(secondString);
+
+        }
+    }
+}
