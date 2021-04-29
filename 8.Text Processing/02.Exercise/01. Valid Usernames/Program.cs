6db8a1b75d48b0a045259ee4106f5083fb1360af
donchodonev <dummyacc9292@gmail.com>
2021-04-29 11:15:20 -0700

    added .docx exercise tasks file added first exercise solution
    Signed-off-by: donchodonev <dummyacc9292@gmail.com>

---
+++ 8.Text Processing/02.Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            for (int i = 0; i < usernames.Count; i++)
+            {
+                string username = usernames[i];
+
+                for (int v = 0; v < username.Length; v++)
+                {
+                    if (username.Length < 3 
+                        || 
+                        username.Length >= 16)
+                    {
+                        usernames.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                    else if (!char.IsLetterOrDigit(username[v]) 
+                        && username[v] != '_' 
+                        && username[v] != '-')
+                    {
+                        usernames.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            foreach (var username in usernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
