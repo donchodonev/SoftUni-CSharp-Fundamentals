f328890a9e8ce55a07b327c08f2906c20fc70bd8
donchodonev <doniodonef@gmail.com>
2021-04-29 10:02:52 +0300

    added new repo folder with next topic added .docx file with exercise tasks added first exercise solution

---
+++ 8.Text Processing/01.Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace _01._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reversedInput = string.Empty;
+
+            while (input != "end")
+            {
+                reversedInput = string.Empty;
+
+                foreach (var character in input.Reverse())
+                {
+                    reversedInput += character.ToString();
+                }
+                Console.WriteLine($"{input} = {reversedInput}");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
