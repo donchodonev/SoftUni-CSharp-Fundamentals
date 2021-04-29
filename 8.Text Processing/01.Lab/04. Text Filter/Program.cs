9feddee695fb6b7da3e73483f76653dc29d8cfe2
donchodonev <doniodonef@gmail.com>
2021-04-29 10:39:39 +0300

    added another exercise solutiom

---
+++ 8.Text Processing/01.Lab/04. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string textToFilter = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                if (textToFilter.Contains(word))
+                {
+                    textToFilter = textToFilter.Replace(word, new string('*', word.Length));
+                }
+            }
+            Console.WriteLine(textToFilter); ;
+        }
+    }
+}
