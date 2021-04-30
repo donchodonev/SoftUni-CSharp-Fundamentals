56989c86491196edc9f7b32024eb155dbb998563
donchodonev <dummyacc9292@gmail.com>
2021-04-30 13:23:55 +0300

    added another exercise solution

---
+++ 8.Text Processing/02.Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+
+            foreach (var character in input)
+            {
+                output += ((char)(character + 3)).ToString();
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
