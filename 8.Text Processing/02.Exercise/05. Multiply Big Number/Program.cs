102ddc3504f87c9ecdbb0e30a4991ae33b2e9e1d
donchodonev <dummyacc9292@gmail.com>
2021-04-30 13:24:10 +0300

    added another exercise solution

---
+++ 8.Text Processing/02.Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = BigInteger.Parse(Console.ReadLine());
+            BigInteger number2 = BigInteger.Parse(Console.ReadLine());
+
+            Console.WriteLine(number * number2);
+
+        }
+    }
+}
