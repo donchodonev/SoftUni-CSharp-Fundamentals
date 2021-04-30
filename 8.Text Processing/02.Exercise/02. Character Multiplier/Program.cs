d417b75eccce4130647f64418df0f001150c9fd0
donchodonev <dummyacc9292@gmail.com>
2021-04-30 13:23:05 +0300

    added another exercise solution

---
+++ 8.Text Processing/02.Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _02._Character_Multiplier
+{
+    class Program
+    {
+        static int Multiplier(string firstString, string secondString)
+        {
+            var sum = 0;
+
+            var longestWord = firstString;
+            var shortestWord = secondString;
+
+            if (firstString.Length < secondString.Length)
+            {
+                shortestWord = firstString;
+                longestWord = secondString;
+            }
+            else if (firstString.Length > secondString.Length)
+            {
+                longestWord = firstString;
+                shortestWord = secondString;
+            }
+
+            for (var i = 0; i < shortestWord.Length; i++)
+            {
+                var multiply = shortestWord[i] * longestWord[i];
+                sum += multiply;
+            }
+
+            for (var v = shortestWord.Length; v < longestWord.Length; v++)
+            {
+                var additon = longestWord[v];
+                sum += additon; ;
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var firstString = input[0];
+
+            var secondString = input[1];
+
+            var sum = Multiplier(firstString, secondString);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
