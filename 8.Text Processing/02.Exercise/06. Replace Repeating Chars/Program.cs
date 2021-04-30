c16d8e76ee610341a3f4d344f362edf0bfa7c2ec
donchodonev <dummyacc9292@gmail.com>
2021-04-30 13:25:16 +0300

    added another exercise solution

---
+++ 8.Text Processing/02.Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length - 1)
+                {
+
+                    output.Append(input[i]);
+
+                    break;
+                }
+                if (input[i] != input[i + 1])
+                {
+                    output.Append(input[i].ToString());
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
