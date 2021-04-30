26942ec536604e1c79f5066505c1df53ec246752
donchodonev <dummyacc9292@gmail.com>
2021-04-30 13:23:42 +0300

    added another exercise solution

---
+++ 8.Text Processing/02.Exercise/03. Extract File/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            int pathLength = path.Length;
+
+            int indexOfLastSlash = path.LastIndexOf('\\');
+
+            string fileNameAndExt = string.Empty;
+
+            string[] splitNameAndExt = path.Substring(indexOfLastSlash + 1, path.Length - indexOfLastSlash - 1).Split('.');
+
+            string fileName = splitNameAndExt[0];
+            string extension = splitNameAndExt[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
