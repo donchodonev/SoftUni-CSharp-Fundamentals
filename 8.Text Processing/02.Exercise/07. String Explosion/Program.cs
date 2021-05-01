4da68018f250607ca18b8ee5b7fee6f926292e7f
donchodonev <dummyacc9292@gmail.com>
2021-05-01 11:10:19 +0300

    added another exercise solution

---
+++ 8.Text Processing/02.Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace _07._String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder output = new StringBuilder();
+
+            int skip = 0;
+
+            for (int i = 0 + skip; i < text.Length; i++)
+            {
+                if (text[i].ToString() == ">")
+                {
+                    skip += int.Parse(text[i+1].ToString());
+
+                    output.Append(text[i].ToString());
+
+                    continue;
+                }
+                else if (skip > 0)
+                {
+                    skip--;
+
+                    continue;
+                }
+                else
+                {
+                    output.Append(text[i].ToString());
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
