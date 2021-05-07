b260b785038c3f06636fbaec984dc156604cd5dc
donchodonev <dummyacc9292@gmail.com>
2021-05-07 12:06:46 +0300

    added final exercise solution

---
+++ 8.Text Processing/03.More Exercise/04. HTML/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace _04._HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = Console.ReadLine();
+            string articleContent = Console.ReadLine();
+
+            string comment = Console.ReadLine();
+
+            Console.WriteLine("<h1>");
+            Console.WriteLine($"{title}");
+            Console.WriteLine("</h1>");
+
+            Console.WriteLine("<article>");
+            Console.WriteLine($"{articleContent}");
+            Console.WriteLine("</article>");
+
+            while (comment != "end of comments")
+            {
+                Console.WriteLine($"<div>");
+                Console.WriteLine(comment);
+                Console.WriteLine($"</div>");
+
+                comment = Console.ReadLine();
+            }
+        }
+    }
+}
