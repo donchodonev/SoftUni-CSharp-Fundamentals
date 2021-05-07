cffe7a8f35026ba32abee386ab5ff2168031844f
donchodonev <dummyacc9292@gmail.com>
2021-05-07 09:51:03 +0300

    added new folder with exercises added exercises tasks .docx file added first exercise solution

---
+++ 8.Text Processing/03.More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //find length between @ and | = name
+            //find length between # and * = age
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                int nameStartIndex = input.IndexOf('@') + 1;
+                int nameEndIndex = input.IndexOf('|');
+                int nameLength = nameEndIndex - nameStartIndex;
+
+                string name = input.Substring(nameStartIndex, nameLength);
+
+                int ageStartIndex = input.IndexOf('#') + 1;
+                int ageEndIndex = input.IndexOf('*');
+                int ageLength = ageEndIndex - ageStartIndex;
+
+                string age = input.Substring(ageStartIndex, ageLength);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
