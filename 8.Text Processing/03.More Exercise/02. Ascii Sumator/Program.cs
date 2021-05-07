90145f041774cac5ca5e9aa97fca9334844df58f
donchodonev <dummyacc9292@gmail.com>
2021-05-07 10:23:26 +0300

    added another exercise solution

---
+++ 8.Text Processing/03.More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstChar = Console.ReadLine().ToCharArray();
+            char[] secondChar = Console.ReadLine().ToCharArray();
+
+            string randomString = Console.ReadLine();
+
+            int startIndex = firstChar[0];
+            int endIndex = secondChar[0];
+
+            int sum = 0;
+
+            foreach (var character in randomString)
+            {
+                if (character > startIndex && character < endIndex)
+                {
+                    sum += character;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
