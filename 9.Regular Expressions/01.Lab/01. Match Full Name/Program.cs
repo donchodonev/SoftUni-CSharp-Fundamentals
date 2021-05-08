1d30197b5f378a8a3e1797f8ff6ddc7f794d8da5
donchodonev <dummyacc9292@gmail.com>
2021-05-08 12:40:36 +0300

    added first exercise solution

---
+++ 9.Regular Expressions/01.Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+\b \b[A-Z][a-z]+\b";
+
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, pattern);
+
+            foreach (Match match in matchedNames)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
