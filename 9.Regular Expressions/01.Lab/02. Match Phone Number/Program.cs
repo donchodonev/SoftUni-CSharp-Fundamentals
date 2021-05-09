7a1b6a5e4735d94890ff1f002b6f96be9aecd418
donchodonev <dummyacc9292@gmail.com>
2021-05-09 11:55:08 +0300

    added another exercise solution

---
+++ 9.Regular Expressions/01.Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359(\s|-)2(\1)\d{3}(\1)\d{4}\b";
+
+            string numbers = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(numbers, pattern);
+
+            Console.WriteLine(String.Join(", ",matches.Select(x => x.Value)));
+        }
+    }
+}
