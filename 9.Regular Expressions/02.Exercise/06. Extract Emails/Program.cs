c0eeed2810509ce4b2f74e14a40196a29a6655db
donchodonev <dummyacc9292@gmail.com>
2021-05-10 15:47:13 +0300

    added another exercise solution

---
+++ 9.Regular Expressions/02.Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> emails = new List<string>();
+
+            string input = Console.ReadLine();
+
+            string emailsPattern = @"\b[A-Za-z0-9]+([\.\-_]?[A-Za-z0-9])*\@[A-Za-z0-9-]{2,}(\.[A-Za-z0-9-]{2,})+\b";
+
+            MatchCollection validEmails = Regex.Matches(input, emailsPattern);
+
+            foreach (Match email in validEmails)
+            {
+                Console.WriteLine(email.Value);
+            }
+        }
+    }
+}
