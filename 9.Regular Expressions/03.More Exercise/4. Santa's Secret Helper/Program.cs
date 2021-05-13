ebfe08180cc723cc05fbcecc6041395851702e66
donchodonev <dummyacc9292@gmail.com>
2021-05-13 10:19:51 +0300

    added final exercise solution for the regular expressions section of the course and the whole course itself

---
+++ 9.Regular Expressions/03.More Exercise/4. Santa's Secret Helper/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _4._Santa_s_Secret_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            Regex nameAndBehaviour = new Regex(@"@(?<name>[A-Za-z][A-Za-z]+)[^\@\-!:>]*!(?<behaviour>[GN])!");
+
+            while (input != "end")
+            {
+                string decryptedMessage = string.Empty;
+
+                foreach (var character in input)
+                {
+                    decryptedMessage += (char)(character - key);
+                }
+
+                if (nameAndBehaviour.Match(decryptedMessage).Success)
+                {
+                    if (nameAndBehaviour.Match(decryptedMessage).Groups["behaviour"].Value == "G")
+                    {
+                        Console.WriteLine($"{nameAndBehaviour.Match(decryptedMessage).Groups["name"].Value}");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
